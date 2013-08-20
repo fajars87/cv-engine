ed6111928c3c5b01001580f799d6af4054f2a3a3
Sergey Grigoriev <s.grigoriev@gmail.com>
2013-08-20 12:38:03 +0300

    I18n rewritten: universal function of locale date has been added

--- sources/I18n.php
+++ sources/I18n.php
@@ -4,6 +4,20 @@
  */
 
 class I18n {
+
+    const LOCALE_UTF8 = 0;
+    const LANGUAGE = 1;
+    const DATE_MONTH_YEAR = 2;
+    const MONTH_YEAR = 3;
+    const PRESENT_MOMENT = 4;
+
+    private static $data = array(
+        //    locale      , language , full       , short  , present moment
+        array('en_US.UTF8', 'english', '%b %d, %Y', '%b %Y', 'Present'),
+        array('de_DE.UTF8', 'german',  '%d.%m.%Y',  '%m.%Y', 'biz heute'),
+        array('ru_RU.utf8', 'russian', '%d.%m.%Y',  '%m.%Y', 'настоящее время')
+    );
+
     private static function runsOnWindows() {
         php_uname();
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
@@ -12,30 +26,26 @@ class I18n {
         return false;
     }
 
-    public static function localizedDate($date, $language) {
+    // format should be specified as an array position
+    public static function localizedDate($date, $language, $format = I18n::MONTH_YEAR) {
         date_default_timezone_set('UTC');
         $result = strtotime($date);
 
+        $lang = 0;
+
         if ($language === Language::LANG_EN) {
-            setlocale(LC_TIME, 'en_US.UTF8', 'english');
-            $result = strftime('%b %Y', strtotime($date));
-            if ($date === '0000-00-00') {
-                $result = 'Present';
-            }
+            $lang = 0;
         } else if ($language === Language::LANG_DE) {
-            setlocale(LC_TIME, 'de_DE.UTF8', 'german');
-            $result = strftime('%m.%Y', strtotime($date));
-            if ($date === '0000-00-00') {
-                $result = 'biz heute';
-            }
+            $lang = 1;
         } else if ($language === Language::LANG_RU) {
-            setlocale(LC_TIME, 'ru_RU.utf8', 'russian');
-            $result = strftime('%m.%Y', strtotime($date));
-            if ($date === '0000-00-00') {
-                $result = 'настоящее время';
-            }
+            $lang = 2;
+        }
+
+        setlocale(LC_TIME, self::$data[$lang][I18n::LOCALE_UTF8], self::$data[$lang][I18n::LANGUAGE]);
+        $result = strftime(self::$data[$lang][$format], strtotime($date));
+        if ($date === '0000-00-00') {
+            $result = self::$data[$lang][I18n::PRESENT_MOMENT];
         }
         return $result;
     }
-
 }
